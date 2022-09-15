67167319ea9eee344742fbef601a2b025888049a
Jonas Degrave <jonasdegrave@poli.ufrj.br>
2022-09-14 21:11:47 -0300

    Fokker 70 fixes

--- designTool.py
+++ designTool.py
@@ -3246,17 +3246,17 @@ def standard_airplane(name="fokker100"):
             "taper_w": 0.28,  # Wing taper ratio
             "sweep_w": 19.9 * np.pi / 180,  # Wing sweep [rad]
             "dihedral_w": 3 * np.pi / 180,  # Wing dihedral [rad]
-            "xr_w": 14.2,  # Longitudinal position of the wing (with respect to the fuselage nose) [m]
-            "zr_w": -0.5,  # Vertical position of the wing (with respect to the fuselage nose) [m]
+            "xr_w": 11.72,  # Longitudinal position of the wing (with respect to the fuselage nose) [m]
+            "zr_w": -1.01,  # Vertical position of the wing (with respect to the fuselage nose) [m]
             "tcr_w": 0.123,  # t/c of the root section of the wing
             "tct_w": 0.096,  # t/c of the tip section of the wing
             "Cht": 0.85,  # Horizontal tail volume coefficient
-            "Lc_h": 3.88,  # Non-dimensional lever of the horizontal tail (lever/wing_mac)
+            "Lc_h": 3.5,  # Non-dimensional lever of the horizontal tail (lever/wing_mac)
             "AR_h": 4.41,  # HT aspect ratio
             "taper_h": 0.42,  # HT taper ratio
             "sweep_h": 27.14 * np.pi / 180,  # HT sweep [rad]
             "dihedral_h": 4.2 * np.pi / 180,  # HT dihedral [rad]
-            "zr_h": 5.6,  # Vertical position of the HT [m]
+            "zr_h": 4.79,  # Vertical position of the HT [m]
             "tcr_h": 0.1,  # t/c of the root section of the HT
             "tct_h": 0.1,  # t/c of the tip section of the HT
             "eta_h": 1.0,  # Dynamic pressure factor of the HT
@@ -3268,13 +3268,13 @@ def standard_airplane(name="fokker100"):
             "zr_v": 1.65,  # Vertical position of the VT [m]
             "tcr_v": 0.1,  # t/c of the root section of the VT
             "tct_v": 0.1,  # t/c of the tip section of the VT
-            "L_f": 30.91,  # Fuselage length [m]
+            "L_f": 27.878,  # Fuselage length [m]
             "D_f": 3.3,  # Fuselage diameter [m]
-            "x_n": 23.2,  # Longitudinal position of the nacelle frontal face [m]
-            "y_n": 2.6,  # Lateral position of the nacelle centerline [m]
-            "z_n": 0.0,  # Vertical position of the nacelle centerline [m]
-            "L_n": 1.70,  # Nacelle length [m]
-            "D_n": 1.6,  # Nacelle diameter [m]
+            "x_n": 17.52,  # Longitudinal position of the nacelle frontal face [m]
+            "y_n": 2.7,  # Lateral position of the nacelle centerline [m]
+            "z_n": 0.45,  # Vertical position of the nacelle centerline [m]
+            "L_n": 5.02,  # Nacelle length [m]
+            "D_n": 1.64,  # Nacelle diameter [m]
             "n_engines": 2,  # Number of engines
             "n_engines_under_wing": 0,  # Number of engines installed under the wing
             "engine": {
@@ -3282,10 +3282,10 @@ def standard_airplane(name="fokker100"):
                 "BPR": 3.04,  # Engine bypass ratio
                 "Cbase": 0.57 / 3600,
             },
-            "x_nlg": 3.92,  # Longitudinal position of the nose landing gear [m]
-            "x_mlg": 17.92,  # Longitudinal position of the main landing gear [m]
+            "x_nlg": 3.64,  # Longitudinal position of the nose landing gear [m]
+            "x_mlg": 15.18,  # Longitudinal position of the main landing gear [m]
             "y_mlg": 2.52,  # Lateral position of the main landing gear [m]
-            "z_lg": -1.41,  # Vertical position of the landing gear [m]
+            "z_lg": -3.05,  # Vertical position of the landing gear [m]
             "x_tailstrike": 23.68,  # Longitudinal position of critical tailstrike point [m]
             "z_tailstrike": -0.84,  # Vertical position of critical tailstrike point [m]
             "c_tank_c_w": 0.4,  # Fraction of the wing chord occupied by the fuel tank
